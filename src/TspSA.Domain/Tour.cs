6d092ec92b8906629e76ad6d3ee00c0cab41acee
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2014-10-28 19:31:52 -0200

    Basic tour model

---
+++ src/TspSA.Domain/Tour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace TspSA.Domain
+{
+    public class Tour
+    {
+        private readonly City[] _route;
+        private double _totalDistance;
+
+        public Tour(params City[] route)
+        {
+            _route = route;
+        }
+
+        public IEnumerable<City> Route { get { return _route; } }
+
+        public double TotalDistance
+        {
+            get
+            {
+                if (_totalDistance == 0)
+                {
+                    for (var i = 0; i < _route.Length; i++)
+                    {
+                        var j = (i + 1)%_route.Length;
+                        _totalDistance += _route[i].DistanceTo(_route[j]);
+                    }
+                }
+                
+                return _totalDistance;
+            }
+        }
+    }
+}
---
+++ tests/TspSA.Domain.Tests/TourFacts.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TspSA.Domain.Tests
+{
+    public class TourFacts
+    {
+        [Fact]
+        public void TotalDistanceReturnsSumOfRouteDistances()
+        {
+            var tour = new Tour(
+                new City(0, 0),
+                new City(3, 4),
+                new City(-3, 4)
+                );
+
+            tour.TotalDistance.Should().Be(16d);
+        }
+    }
+}
