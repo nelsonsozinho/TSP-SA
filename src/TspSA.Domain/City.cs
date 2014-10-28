d92b375977d022e9c8509e0a2bd51159a439b249
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2014-10-28 17:12:20 -0200

    Basic city representation

---
+++ src/TspSA.Domain/City.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TspSA.Domain
+{
+    public class City
+    {
+        private readonly double _locationX;
+        private readonly double _locationY;
+
+        public City(double locationX, double locationY)
+        {
+            _locationX = locationX;
+            _locationY = locationY;
+        }
+
+        public double LocationX
+        {
+            get { return _locationX; }
+        }
+
+        public double LocationY
+        {
+            get { return _locationY; }
+        }
+
+        public double DistanceTo(City other)
+        {
+            var dx = other.LocationX - LocationX;
+            var dy = other.LocationY - LocationY;
+
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
---
+++ tests/TspSA.Domain.Tests/CityFacts.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TspSA.Domain.Tests
+{
+    public class CityFacts
+    {
+        [Fact]
+        public void DistanceOfACityIn_0_0_ToAnotherIn_3_4_Is_5()
+        {
+            var city1 = new City(0, 0);
+            var city2 = new City(3, 4);
+            city1.DistanceTo(city2).Should().Be(5);
+        }
+    }
+}
